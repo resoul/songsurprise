99c7e3656b1f51b990f73b66f4bb3306d108d8c7
resoul <resoul.ua@icloud.com>
2024-09-11 23:42:17 +0300

    init

---
+++ songsurprise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  songsurprise
+//
+//  Created by resoul on 11.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
